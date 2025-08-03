83eca6b99a78118272ff84f4f2c1f1624f041396
Shreya agrawal <shreyaagrawal1259@gmail.com>
2025-08-03 22:22:40 +0530

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BookingForm from "./components/BookingForm";
+import "./styles.css";
+
+function App() {
+  return (
+    <main>
+      <BookingForm />
+    </main>
+  );
+}
+
+export default App;
